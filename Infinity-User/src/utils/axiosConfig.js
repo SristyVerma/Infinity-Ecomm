9336cb6e831a8bdb2318e28a34214fc8f5f521f5
sristy verma <sristyel2016@gmail.com>
2023-05-30 17:25:09 +0530

    added

--- Infinity-User/src/utils/axiosConfig.js
+++ Infinity-User/src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ export const base_url = "https://cooperative-lion-sneakers.cyclic.app/api/";
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
   : null;
-
+// hj
 export const config = {
   headers: {
     Authorization: `Bearer ${
